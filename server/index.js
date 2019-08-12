30aa38bc9f073db1986676d3f2a480ffedbf3e20
Jay Lin <jay.lin@xero.com>
2019-08-12 16:48:10 -0700

    CRUD Book

---
+++ server/index.js
@@ -0,0 +1,46 @@
+require('dotenv').config();
+const { importSchema } = require('graphql-import');
+const { ApolloServer, gql } = require('apollo-server');
+const opn = require('opn');
+const PORT = process.env.port || 3000;
+const {posts, books} = require('./model');
+
+// console.log(process.env);
+
+// Type definitions define the "shape" of your data and specify
+// which ways the data can be fetched from the GraphQL server.
+const typeDefs = gql`${importSchema(__dirname + "/schema/schema.graphql")}`;
+
+// Resolvers define the technique for fetching the types in the
+// schema.  We'll retrieve books from the "books" array above.
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+  Mutation: {
+    book: (parent, args) => {
+      console.log('data', args);
+      books.push({
+        ...args,
+        id: Date.now().toString().substr(-4)
+      });
+      return books;
+    }
+  }
+};
+
+// In the most basic sense, the ApolloServer can be started
+// by passing type definitions (typeDefs) and the resolvers
+// responsible for fetching the data for those types.
+const server = new ApolloServer({
+  // typeDefs: __dirname + '/schema/schema.graphql',
+  typeDefs,
+  resolvers
+});
+
+// This `listen` method launches a web-server.  Existing apps
+// can utilize middleware options, which we'll discuss later.
+server.listen({port: PORT}).then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+  // opn(url);
+});
---
+++ server/model/index.js
@@ -0,0 +1,33 @@
+const fetch = require("node-fetch");
+
+// This is a (sample) collection of books we'll be able to query
+// the GraphQL server for.  A more complete example might fetch
+// from an existing data source like a REST API or database.
+const books = [
+  {
+    id: 'abc',
+    title: 'Harry Potter and the Chamber of Secrets',
+    author: {
+      firstName: 'J.K.',
+      lastName: 'Rowling'
+    }
+  },
+  {
+    id: '123',
+    title: 'Jurassic Park',
+    author: {
+      firstName: 'Michael',
+      lastName: 'Crichton'
+    },
+  },
+];
+
+async function placeholderPosts() {
+  const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+  return response.json();
+}
+
+module.exports = {
+  posts: placeholderPosts(),
+  books
+};
